836fcd637853814e32aa37975658d3ada62474ef
“Jasongit config --global user.email “jasonsutter87@gmail.com <jasonsutter@jasonsMsomeness.wavecable.com>
2015-11-15 15:53:55 -0800

    Added Paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115233951) do
+ActiveRecord::Schema.define(version: 20151115234521) do
 
   create_table "blogs", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "title"
     t.string   "url"
     t.text     "post"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "blogs", ["user_id"], name: "index_blogs_on_user_id"
